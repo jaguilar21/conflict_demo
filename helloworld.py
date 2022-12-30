9221ab9c3c0ae04fb93ca5337aae5fc9f295ffa5
Jesse Aguilar <jaguilar12@comcast.net>
2022-12-29 21:20:44 -0700

    merge

--- helloworld.py
+++ helloworld.py
@@ -5,14 +5,6 @@ def hello_world(city,state):
 
 def main():
     hello_world('NYC','New York')
-=======
-def hello_world(city):
-    print(f'Hello World {city}!')
-
-
-def main():
-    hello_world('NYC')
->>>>>>> 1b80e0310399f37a3f4a2d4c9c18d4371b574a18
 
 
 main()
